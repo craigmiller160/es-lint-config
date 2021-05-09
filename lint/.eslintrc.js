80a081c6684dcfb161f7259542b5e46393696f49
Craig Miller <craigmiller160@gmail.com>
2021-05-09 13:38:28 -0400

    Integrated vue into lint settings

--- lint/.eslintrc.js
+++ lint/.eslintrc.js
@@ -1,6 +1,8 @@
 const baseConfig = require('./.eslintrc.base');
 const reactConfig = require('./.eslintrc.react');
 const tsConfig = require('./.eslintrc.typescript');
+const vueBaseConfig = require('./.eslintrc.vue-base');
+const vueTsConfig = require('./.eslintrc.vue-ts');
 const deepmerge = require('deepmerge');
 
 let config;
@@ -16,6 +18,12 @@ switch (process.env.ES_LINT_CONFIG_TYPE) {
         break;
     case 'react-ts':
         config = deepmerge(deepmerge(baseConfig, tsConfig), reactConfig);
+        break;
+    case 'vue-js':
+        config = deepmerge(baseConfig, vueBaseConfig);
+        break;
+    case 'vue-ts':
+        config = deepmerge(deepmerge(baseConfig, vueBaseConfig), vueTsConfig);
         break;
     default:
         throw new Error(`Invalid es-lint-config type: ${process.env.ES_LINT_CONFIG_TYPE}`);
