6ea924b7dbe03436b79aeb33aeb46fb88710d575
Craig Miller <craigmiller160@gmail.com>
2021-05-09 15:12:57 -0400

    Got a combineMerge function concept working

--- test.js
+++ test.js
@@ -1,15 +1,54 @@
+// TODO delete this file
+
 const deepmerge = require('deepmerge');
 
 const abc = {
-    'one': 'two'
+    'one': 'two',
+    array: [
+        {
+            'type': 'array1',
+            name: 'bob'
+        }
+    ]
 };
 
 const def = {
-    'three': 'four'
+    'three': 'four',
+    array: [
+        {
+            type: 'array1',
+            name: 'john'
+        }
+    ]
 };
 
 const ghi = {
-    'five': 'six'
+    'five': 'six',
+    array: [
+        {
+            type: 'array2',
+            name: 'kim'
+        }
+    ]
 };
 
-console.log(deepmerge(deepmerge(abc, def), ghi));
+const combineMerge = (target, source, options) => {
+    const destination = target.slice();
+
+    source.forEach((item, index) => {
+        const targetIndex = destination.findIndex((destItem) => destItem.type === item.type);
+        if (targetIndex >= 0) {
+            destination[targetIndex] = deepmerge(destination[targetIndex], item, options);
+        } else {
+            destination.push(item);
+        }
+    });
+
+    return destination;
+};
+
+const options = {
+    arrayMerge: combineMerge
+};
+
+console.log(deepmerge(deepmerge(abc, def, options), ghi, options));
