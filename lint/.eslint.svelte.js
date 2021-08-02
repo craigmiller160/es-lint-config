2f53ceea3d96aaf36bf1b5d348a8eb0a48479d52
Craig Miller <craigmiller160@gmail.com>
2021-08-01 20:56:25 -0400

    Working on svelete support

---
+++ lint/.eslint.svelte.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+    overrides: [
+        {
+            files: [
+                '*.svelte'
+            ],
+            processor: 'svelte3/svelte3'
+        }
+    ],
+    plugins: [
+        'svelte3'
+    ],
+    settings: {
+        'svelte3/typescript': require('typescript'),
+        'svelte3/ignore-styles': () => true
+    }
+};
