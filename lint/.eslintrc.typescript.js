c74452a3c80c299227c0f0eacb2e92a7f9375636
Craig Miller <craigmiller160@gmail.com>
2021-08-01 21:18:11 -0400

    More tweaks

--- lint/.eslintrc.typescript.js
+++ lint/.eslintrc.typescript.js
@@ -5,13 +5,22 @@ module.exports = {
         {
             files: ['**/*.ts?(x)'],
             parser: '@typescript-eslint/parser',
-            extends: ['plugin:@typescript-eslint/recommended'],
+            extends: [
+                'plugin:@typescript-eslint/recommended',
+                'plugin:@typescript-eslint/recommended-requiring-type-checking'
+            ],
             settings: {
+                // TODO remove this if the parserOptions stuff stays
                 'import/resolver': {
                     typescript: {
                         project: path.resolve(process.cwd(), 'tsconfig.json')
                     }
                 }
+            },
+            parserOptions: {
+                // TODO concerned about this in other projects
+                tsconfigRootDir: process.cwd(),
+                project: './tsconfig.json'
             },
             rules: {
                 '@typescript-eslint/no-unused-vars': 'error',
